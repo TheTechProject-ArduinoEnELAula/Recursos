9c04a9ab1d125e9cd5490b36dc3ef81abaac9172
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2018-01-15 17:13:19 +0100

    Create Hola_Mundo.ino

---
+++ U2/2.5_HolaMundo/Hola_Mundo/Hola_Mundo.ino
@@ -0,0 +1,23 @@
+/* 
+ Blink- Titilar 
+ Enciende y apaga LED por espacio de un segundo de forma repetitiva 
+ Jose Pujol
+ Octubre 2017
+ */
+
+// declaracion de variables 
+int ledPin=13; // LED asociado al pin 13      
+
+// la funcion setup se ejecuta una sola vez 
+void setup() { 
+  pinMode(ledPin, OUTPUT); // inicializa el pin 13 como una salida digital 
+} 
+
+// la funcion loop se ejecuta repetidamente de forma infinita 
+void loop() { 
+  digitalWrite(ledPin, HIGH); // establece 5v en el pin del LED 
+  delay(1000); // espera un segundo 
+  digitalWrite(ledPin, LOW); // establece 0v en el pin del LED 
+  delay(1000); // espera un segundo 
+}
+
