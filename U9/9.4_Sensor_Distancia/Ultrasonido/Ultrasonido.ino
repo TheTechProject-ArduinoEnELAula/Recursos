a1df517d7adc18b6ec97fbad58f86a4c5eb5b447
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2018-01-16 21:20:11 +0100

    Create Ultrasonido.ino

---
+++ U9/9.4_Sensor_Distancia/Ultrasonido/Ultrasonido.ino
@@ -0,0 +1,48 @@
+// Conexion pines del sensor de distancia
+const int triggerPin = 11; // Pin donde conectamos el emisor
+const int echoPin = 12; // Pin donde conectamos el receptor
+
+// variables para almacenar tiempo actual y tiempo anterior
+unsigned long current_time = 0;
+unsigned long previous_time = 0;
+
+void setup() {
+  Serial.begin(9600); // Abrimos el puerto serie
+  pinMode(triggerPin, OUTPUT); // Configuramos trigger como salida digital
+  pinMode(echoPin, INPUT); // Configuramos echo como entrada digital
+}
+void loop() {
+  // grabamos el tiempo
+  current_time = millis();
+  // si el tiempo es mayor que el intervalo medimos
+  if (current_time - previous_time > 1000) {
+    previous_time = current_time;
+    float cm = measuringdistance ();
+    Serial.println (cm);
+  }
+}
+
+// funcion para medir la distancia
+float  measuringdistance() {
+  // Variable para almacenar el tiempo de la onda y la distancia
+  float duration, distance;
+  //Inicializamos el sensor
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(5);
+  // Enviamos una señal activando la salida trigger durante 10 microsegundos
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  // Medimos el ancho del pulso, cuando la lectura sea HIGH
+  // devuelve el tiempo transcurrido en microsegundos
+  duration = pulseIn(echoPin, HIGH, 15000);
+  // Calculamos la distancia en cm y la guardamos en distance
+  distance = duration * 0.01715;
+  // devolvemos la distancia
+  return distance;
+}
+
+
+
+
+
