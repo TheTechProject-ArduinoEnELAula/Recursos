028a92787c7d5d0fb988b1f03684d49537343f7f
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2018-02-02 12:13:20 +0100

    Update Display-7-1.ino

--- U09/9.2_Display-7-Segmentos/Display-7-1/Display-7-1.ino
+++ U09/9.2_Display-7-Segmentos/Display-7-1/Display-7-1.ino
@@ -7,16 +7,16 @@
    Jose Pujol
 */
 
-const int pinLatch = 11;  //Pin para el latch de los 74CH495
-const int pinData = 10;  //Pin para Datos serie del 74CH495
-const int pinClock = 12;  //Pin para reloj del 74CH495
+const int pinLatch = 11;  // pin para el latch del 74CH495
+const int pinData = 10;  // pin para Datos serie del 74CH495
+const int pinClock = 12;  // pin para reloj del 74CH495
 
 // Codificación binaria de los dígitos en el display de 7 segmentos
 byte numbers [10] = {B00000011, B10011111, B00100101, B00001101,B10011001,
                      B01001001, B01000001, B00011111, B00000001, B00001001};
 
 void setup() {
-  // declaramos los pines del latch, datos y el reloj
+  // declaramos los pines del latch, datos y el reloj como salidas
   pinMode(pinLatch, OUTPUT);
   pinMode(pinData, OUTPUT);
   pinMode(pinClock, OUTPUT);
